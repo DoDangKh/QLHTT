379ba83b40934f14dc765d84390387f57f1655a7
Khoa <ddangkhoa75@gmail.com>
2022-12-23 00:31:17 -0800

    Forgot Password

--- src/main/java/com/example/qlhtt/Controller/ProductController.java
+++ src/main/java/com/example/qlhtt/Controller/ProductController.java
@@ -3,6 +3,9 @@ package com.example.qlhtt.Controller;
 import com.example.qlhtt.Entity.Product;
 import com.example.qlhtt.Repos.ProductRepos;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +20,11 @@ public class ProductController {
     @Autowired
     private ProductRepos productRepos;
 
-    @GetMapping("/all")
-    public ResponseEntity<?> getall(){
-        List<Product> products= productRepos.getall();
-        return ResponseEntity.ok(products);
-    }
+//    @GetMapping("/all")
+//    public ResponseEntity<?> getall(){
+//        List<Product> products= productRepos.getall();
+//        return ResponseEntity.ok(products);
+//    }
     @GetMapping("/{id}")
     public ModelAndView ProductDetail(@PathVariable int id){
         Product product= productRepos.getid(id);
@@ -29,5 +32,20 @@ public class ProductController {
         mav.addObject("p",product);
         return mav;
     }
-
+    @RequestMapping("/type/{id}/page/{p}")
+    public ModelAndView all(@PathVariable("p") int p,@PathVariable("id") Long id){
+        Pageable pageable= PageRequest.of(p,10);
+        Page<Product> page=productRepos.getPagebytpye(pageable,id);
+        ModelAndView mav=new ModelAndView("listproduct");
+        mav.addObject(page);
+        return mav;
+    }
+    @RequestMapping("/search/page/{p}")
+    public ModelAndView search(@RequestParam("name")String name,@PathVariable("p") int p){
+        Pageable pageable=PageRequest.of(p,10);
+        Page<Product> page=productRepos.getPagebystring(pageable,name);
+        ModelAndView mav=new ModelAndView("listproduct");
+        mav.addObject(page);
+        return mav;
+    }
 }
